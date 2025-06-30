125fafe8a0c77ca23dec52cfa9c956e3ea58c29b
jx <jxaop1@gmail.com>
2025-06-30 20:04:51 -0300

    Add files via upload

--- string/script.js
+++ string/script.js
@@ -28,3 +28,16 @@ function processaTexto(texto) {
 
     return ordenadas.slice(0, 10);
 }
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return frequencias;
+}
